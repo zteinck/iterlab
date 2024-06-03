07521c2139d952efa5ad00f0f519e11d1da852fb
Zachary Einck <zacharyeinck@gmail.com>
2024-06-02 22:54:16 -0500

    updated deprecated repo name references

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# iterkit
+# iterlab
 
 Collection of helper functions that perform operations on iterables.
--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .iterkit import *
+from .iterlab import *
 
-__version__ = '1.0'
+__version__ = '0.1.0'
 __author__ = 'Zachary T. Einck'
